26b6b23dda0a247dbe0962c3048971788b67faf0
Nagy Attila <nagyatka@gmail.com>
2018-10-26 14:42:20 +0200

    PdfResponse supports inline and attachment content-disposition too.

--- src/Kodiak/Response/PdfResponse.php
+++ src/Kodiak/Response/PdfResponse.php
@@ -5,13 +5,13 @@ namespace Kodiak\Response;
 
 class PdfResponse extends Response
 {
-    public function __construct($content = '', $filename = '')
+    public function __construct($content = '', $filename = '', $disposition = 'inline')
     {
         $headers = [
             "Content-Type: application/pdf",
             "Content-Transfer-Encoding: Binary",
             "Content-Length:".strlen($content),
-            "Content-Disposition: attachment; filename=$filename"
+            "Content-Disposition: $disposition; filename=$filename"
         ];
         parent::__construct($content, 200, $headers);
     }
