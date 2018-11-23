f420aec44b479d2b0a148dcc5ce05fe02f306907
Nagy Attila <nagyatka@gmail.com>
2018-11-23 17:53:33 +0100

    Improved JsonResponse

--- src/Kodiak/Response/JsonResponse.php
+++ src/Kodiak/Response/JsonResponse.php
@@ -3,6 +3,8 @@
 namespace Kodiak\Response;
 
 
+use PandaBase\Record\InstanceRecord;
+
 class JsonResponse extends Response
 {
     /**
@@ -13,6 +15,26 @@ class JsonResponse extends Response
      */
     public function __construct(array $values,$options = JSON_NUMERIC_CHECK, $status = 200)
     {
+        /*
+         * If 'values' array contains InstanceRecord, it will be replaced with instance's getAll() method call result.
+         */
+        function recursiveCheck(&$array) {
+            $r = array();
+            foreach ($array as $key => &$item) {
+                if(is_array($item)) {
+                    $r[$key] = recursiveCheck($item);
+                }
+                elseif ($item instanceof InstanceRecord) {
+                    $t = $item->getAll();
+                    $r[$key] = recursiveCheck($t);
+                } else {
+                    $r[$key] = &$item;
+                }
+            }
+            return $r;
+        }
+        $values = recursiveCheck($values);
+
         parent::__construct(
             json_encode($values, $options),
             $status,
