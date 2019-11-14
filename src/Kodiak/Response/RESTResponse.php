3731f3c7a68ea9f625376b7cc763c87b063666af
Gyurman Attila <attila.gyurman@ilx.hu>
2019-11-14 16:55:46 +0100

    Update RESTResponse.php
    JsonResponse options to RestResponse

--- src/Kodiak/Response/RESTResponse.php
+++ src/Kodiak/Response/RESTResponse.php
@@ -19,19 +19,19 @@ class RESTResponse extends JsonResponse
      * @param bool $result_type
      * @param array|null $content
      */
-    public function __construct($result_type, $content = null)
+    public function __construct($result_type, $content = null, $options = JSON_NUMERIC_CHECK)
     {
         if($result_type == RESTResponse::SUCCESS) {
             parent::__construct([
                 'success'   => RESTResponse::SUCCESS,
                 'data'      => $content
-            ]);
+            ], $options);
         }
         else {
             parent::__construct([
                 'success'   => RESTResponse::ERROR,
                 'error'      => $content
-            ]);
+            ], $options);
 
         }
 
