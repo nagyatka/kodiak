dc10b9514cbf2a367ecade25c51a110067689865
Nagy Attila <nagyatka@gmail.com>
2020-05-01 11:53:52 +0200

    Updated cronrequest

--- src/Kodiak/Request/CronRequest.php
+++ src/Kodiak/Request/CronRequest.php
@@ -2,12 +2,36 @@
 namespace Kodiak\Request;
 
 
+use Kodiak\Application;
+use Kodiak\Core\KodiConf;
+
 class CronRequest extends Request
 {
+    /**
+     * @var array
+     */
+    private $cron_configuration;
+
+    /**
+     * CronRequest constructor.
+     * @param array $cron_configuration
+     */
+    public function __construct(array $cron_configuration)
+    {
+        $this->cron_configuration = $cron_configuration;
+    }
+
+
     public static function get(): Request
     {
         if(!self::$instance){
-            self::$instance = new CronRequest();
+            $env = Application::get(KodiConf::ENVIRONMENT);
+            if(array_key_exists("cron", $env)) {
+                self::$instance = new CronRequest($env["cron"]);
+            }
+            else {
+                self::$instance = new CronRequest([]);
+            }
         }
         return self::$instance;
     }
@@ -18,8 +42,16 @@ class CronRequest extends Request
         return "CRON";
     }
 
+    /**
+     * @return string
+     * @throws \Exception
+     */
     public function getUri(): string
     {
+        if(array_key_exists("uri", $this->cron_configuration)) {
+            return $this->cron_configuration["uri"];
+        }
+
         if(count($_SERVER['argv']) < 2) {
             throw new \Exception("Missing argument");
         }
