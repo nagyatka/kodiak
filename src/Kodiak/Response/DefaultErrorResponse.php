bfbca2df737cedbd8abab46a70ec6f1218ee7425
Nagy Attila <nagyatka@gmail.com>
2020-12-07 18:18:02 +0100

    Type fixes in error response interface

--- src/Kodiak/Response/DefaultErrorResponse.php
+++ src/Kodiak/Response/DefaultErrorResponse.php
@@ -10,32 +10,32 @@ use Kodiak\Request\Request;
 class DefaultErrorResponse implements ErrorResponse
 {
 
-    public function error_401(Request $request, \Exception $exception): Response
+    public function error_401(Request $request, \Throwable $exception): Response
     {
         return new Response(Response::$statusTexts[401],401);
     }
 
-    public function error_403(Request $request, \Exception $exception): Response
+    public function error_403(Request $request, \Throwable $exception): Response
     {
         return new Response(Response::$statusTexts[403],403);
     }
 
-    public function error_404(Request $request, \Exception $exception): Response
+    public function error_404(Request $request, \Throwable $exception): Response
     {
         return new Response(Response::$statusTexts[404],404);
     }
 
-    public function error_500(Request $request, \Exception $exception): Response
+    public function error_500(Request $request, \Throwable $exception): Response
     {
         return new Response(Response::$statusTexts[500],500);
     }
 
-    public function error_503(Request $request, \Exception $exception): Response
+    public function error_503(Request $request, \Throwable $exception): Response
     {
         return new Response(Response::$statusTexts[503],503);
     }
 
-    public function custom_error(Request $request, \Exception $exception): Response
+    public function custom_error(Request $request, \Throwable $exception): Response
     {
         if(Application::getEnvMode() == KodiConf::ENV_DEVELOPMENT){
             $result = $exception->getMessage()."<br>".str_replace("#","<br>#",$exception->getTraceAsString());
--- src/Kodiak/Response/ErrorResponse.php
+++ src/Kodiak/Response/ErrorResponse.php
@@ -11,52 +11,52 @@ interface ErrorResponse
      * Authorization Required
      *
      * @param Request $request
-     * @param \Exception $exception
+     * @param \Throwable $exception
      * @return Response
      */
-    public function error_401(Request $request, \Exception $exception): Response;
+    public function error_401(Request $request, \Throwable $exception): Response;
 
     /**
      * Forbidden
      *
      * @param Request $request
-     * @param \Exception $exception
+     * @param \Throwable $exception
      * @return Response
      */
-    public function error_403(Request $request, \Exception $exception): Response;
+    public function error_403(Request $request, \Throwable $exception): Response;
 
     /**
      * Not found
      *
      * @param Request $request
-     * @param \Exception $exception
+     * @param \Throwable $exception
      * @return Response
      */
-    public function error_404(Request $request, \Exception $exception): Response;
+    public function error_404(Request $request, \Throwable $exception): Response;
 
     /**
      * Internal Server Error
      *
      * @param Request $request
-     * @param \Exception $exception
+     * @param \Throwable $exception
      * @return Response
      */
-    public function error_500(Request $request, \Exception $exception): Response;
+    public function error_500(Request $request, \Throwable $exception): Response;
 
     /**
      * Service Temporarily Unavailable
      *
      * @param Request $request
-     * @param \Exception $exception
+     * @param \Throwable $exception
      * @return Response
      */
-    public function error_503(Request $request, \Exception $exception): Response;
+    public function error_503(Request $request, \Throwable $exception): Response;
 
     /**
      * Custom exception handling
      * @param Request $request
-     * @param \Exception $exception
+     * @param \Throwable $exception
      * @return Response
      */
-    public function custom_error(Request $request, \Exception $exception): Response;
+    public function custom_error(Request $request, \Throwable $exception): Response;
 }
